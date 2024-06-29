7017a74638a412d9e34735199ff47a6598ec6882
Nebelung <nebelung@mailfence.com>
2024-06-29 07:14:18 +0000

    Add minify plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import MillionLint from '@million/lint';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { ViteMinifyPlugin } from 'vite-plugin-minify';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), MillionLint.vite()]
+  plugins: [react(), MillionLint.vite(), ViteMinifyPlugin()]
 });
